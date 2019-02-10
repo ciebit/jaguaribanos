6c6613d6fdb81297c8b4b1617a38e116e3ae7808
Johnny Saymon <saymon@ciebit.com.br>
2019-02-10 18:04:00 -0300

    Criacao de estiliazacao da pagina inicial

--- app/views/commons/template.blade.php
+++ app/views/commons/template.blade.php
@@ -4,10 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-        <title>Jaguaribanos</title>
+        <title>@yield('title')Jaguaribanos</title>
 
         <meta name="author" content="Ciebit">
         <meta name="description" content="Personalidades da cidade de Jaguaribe, Ceará, Brasil">
+        @stack('meta')
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pridi"/>
+        @stack('style')
+        @stack('script')
     </head>
     <body>
         @yield('content')
--- app/views/cover/index.blade.php
+++ app/views/cover/index.blade.php
@@ -1,14 +1,18 @@
 @extends('commons.template')
 
+@push('style')
+    <link rel="stylesheet" href="/assets/css/cover-v1.css"/>
+@endpush
+
 @section('content')
-    <main>
+    <main class="content">
         <article>
-            <h1 class="logo">Jaguaribanos</h1>
+            <img src="/assets/images/logo.png" alt="Jaguaribanos">
             <nav class="menu">
                 <h2 class="menuTitle">Menu</h2>
-                <ul>
-                    <li><a class="menuLink" href="/sobre/">Sobre</a></li>
-                    <li><a class="menuLink" href="/personalidades/">Personalidades</a></li>
+                <ul class="menuItems">
+                    <li class="menuItem"><a class="menuLink" href="/sobre/">Sobre</a></li>
+                    <li class="menuItem"><a class="menuLink" href="/personalidades/">Personalidades</a></li>
                 </ul>
             </nav>
         </article>
